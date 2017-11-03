71966525f84f6098a66f50b8336eee6e6f8898ce
Mac17369 <Mac17369@uvg.edu.gt>
2017-11-03 10:13:41 -0600

    Cambios

--- Lab_5/src/ListaTanques.java
+++ Lab_5/src/ListaTanques.java
@@ -1,4 +1,11 @@
 
 public class ListaTanques {
 
+	private Tanques[] ListaTanques;
+	private int cantTanques;
+	
+	public ListaTanques() {
+		ListaTanques = new Tanques[10];
+	}
+	
 }
--- Lab_5/src/Valvula.java
+++ Lab_5/src/Valvula.java
@@ -1,6 +1,26 @@
 
 public class Valvula {
 
-	private
+	private boolean estado;
+	
+	public Valvula (){
+		estado = true;
+	}
+	
+	public void cerrar() {
+		estado = false;
+	}
+	
+	public void abrir() {
+		estado = true;
+	}
+
+	public boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
 	
 }
